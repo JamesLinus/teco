0257bf71f74d366881bd1a5db881d629a8b3898e
Andy Valencia <ajv-899-334-8894@vsta.org>
1990-12-17 13:52:00 +0000

    Fix formatting

--- te_main.c
+++ te_main.c
@@ -1,6 +1,8 @@
 /* TECO for Ultrix   Copyright 1986 Matt Fichtenbaum						*/
-/* This program and its components belong to GenRad Inc, Concord MA 01742	*/
-/* They may be copied if this copyright notice is included					*/
+/*
+ * This program and its components belong to GenRad Inc, Concord MA 01742
+ * They may be copied if this copyright notice is included
+ */
 
 /* te_main.c  main program  1/14/87 */
 
@@ -62,42 +64,76 @@ char *argv[];		/* array of string pointers */
 	get_term_par();
 
 	/* set up error restart */
-	if (err = setjmp(xxx))
-	{
-		if (err == E_EFI) goto quit;		/* EOF from standard input - clean up and exit */
+	if (err = setjmp(xxx)) {
+		/* EOF from standard input - clean up and exit */
+		if (err == E_EFI)
+			goto quit;
 		printf("\015\012?  %s", errors[err-1]);
-		if (err == E_SRH) print_string(SERBUF);			/* print unfulfilled search string */
-		else if ((err == E_FNF) || (err == E_COF) || (err == E_AMB)) print_string(FILBUF);	/* or file string */
+
+		/* print unfulfilled search string */
+		if (err == E_SRH)
+			print_string(SERBUF);
+
+		/* or file string */
+		else if ((err == E_FNF) || (err == E_COF) ||
+				(err == E_AMB))
+			print_string(FILBUF);
 		crlf();
-		eisw = 0;				/* stop indirect command execution */
-		et_val &= ~(ET_CTRLC | ET_NOWAIT | ET_CTRLO | ET_NOECHO);	/* reset ^C trap, read w/o wait, ^O (unused), no echo */
-		if (et_val & ET_QUIT)	/* if ET has "quit on error" set, exit (phone home) */
-		{
-			cleanup();			/* reset screen, keyboard, output files */
-			exit(1);			/* and exit */
+
+		/* stop indirect command execution */
+		eisw = 0;
+
+		/* reset ^C trap, read w/o wait, ^O (unused), no echo */
+		et_val &= ~(ET_CTRLC | ET_NOWAIT | ET_CTRLO | ET_NOECHO);
+
+		/* if ET has "quit on error" set, exit (phone home) */
+		if (et_val & ET_QUIT) {
+			/* reset screen, keyboard, output files */
+			cleanup();
+			exit(1);		/* and exit */
 		}
 	}
 
 	/* forever: read and execute command strings */
 
-	for (exitflag = 1; exitflag >= 0; )		/* "exit" sets exitflag to -1; ^C to -2; "hangup" to -3 */
-	{
-		window(WIN_REFR);				/* display the buffer */
-		free_blist(insert_p);			/* free any storage from failed insert */
-		free_blist(dly_freebuff);		/* return any delayed cells */
+	/* "exit" sets exitflag to -1; ^C to -2; "hangup" to -3 */
+	for (exitflag = 1; exitflag >= 0; ) {
+		/* display the buffer */
+		window(WIN_REFR);
+
+		/* free any storage from failed insert */
+		free_blist(insert_p);
+
+		/* return any delayed cells */
+		free_blist(dly_freebuff);
 		insert_p = dly_freebuff = NULL;
-		et_val &= ~ET_QUIT;				/* clear "abort on error" */
-		if (read_cmdstr()) goto quit;
-		exitflag = 0;					/* enable ^C detector */
-		if (!WN_scroll) window(WIN_REDRAW);		/* if not in scroll mode, force full redraw on first ^W or nW */
+
+		/* clear "abort on error" */
+		et_val &= ~ET_QUIT;
+		if (read_cmdstr())
+			goto quit;
+
+		/* enable ^C detector */
+		exitflag = 0;
+
+		/*
+		 * If not in scroll mode, force full redraw
+		 * on first ^W or nW
+		 */
+		if (!WN_scroll)
+			window(WIN_REDRAW);
 		exec_cmdstr();
 	}
 
-	if (exitflag == -2) ERROR(E_XAB);		/* ^C detected during execution */
-	else if (exitflag == -3) panic();		/* hangup during execution: save buffer and close files */
+	/* ^C detected during execution */
+	if (exitflag == -2)
+		ERROR(E_XAB);
+
+	/* hangup during execution: save buffer and close files */
+	else if (exitflag == -3)
+		panic();
 
 	/* exit from program */
-
 quit:
 	ev_val = es_val = 0;	/* no last one-line window */
 	window(WIN_REFR);	/* last display */
@@ -109,8 +145,8 @@ quit:
 
 cleanup()
 {
-	window(WIN_OFF);			/* restore screen */
-	setup_tty(TTY_OFF);			/* restore terminal */
+	window(WIN_OFF);		/* restore screen */
+	setup_tty(TTY_OFF);		/* restore terminal */
 	kill_output(&po_file);		/* kill any open primary output file */
 	kill_output(&so_file);		/* and secondary file */
 }
@@ -126,42 +162,50 @@ int arg;
 	struct buffcell *p;
 
 	type_char('"');
-	for (p = qreg[arg].f, c = 0, i = 0; i < qreg[arg].z; i++)
-	{
+	for (p = qreg[arg].f, c = 0, i = 0; i < qreg[arg].z; i++) {
 		if (!p->ch[c]) break;
 		type_char(p->ch[c]);
-		if (++c > CELLSIZE-1)
-		{
+		if (++c > CELLSIZE-1) {
 			p = p->f;
 			c = 0;
 		}
 	}
 	type_char('"');
 }
-/* copy invocation command line to a text buffer */
 
+/* copy invocation command line to a text buffer */
 save_args(argc, argv, q)
-int argc;
-char *argv[];
-struct qh *q;
+	int argc;
+	char *argv[];
+	struct qh *q;
 {
 	char c;
 	struct qp ptr;
 
-	make_buffer(q);				/* attach a text buffer */
-	ptr.p = q->f;				/* initialize pointer to output string */
-	ptr.c = q->z = 0;			/* and output char count */
-	for (; argc > 0; argv++, argc--)	/* for each arg */
-	{
-		while ( ((c = *((*argv)++)) != '\0') && (q->z < CELLSIZE-1) )
-		{
-			ptr.p->ch[ptr.c] = c;		/* copy char to q-reg */
+	/* attach a text buffer */
+	make_buffer(q);
+
+	/*
+	 * Initialize pointer to output string and output char count
+	 * for each argument.
+	 */
+	ptr.p = q->f;
+	ptr.c = q->z = 0;
+	for (; argc > 0; argv++, argc--) {
+		while ( ((c = *((*argv)++)) != '\0') && (q->z < CELLSIZE-1) ) {
+			/* copy char to q-reg */
+			ptr.p->ch[ptr.c] = c;
 			fwdcx(&ptr);
-			++q->z;						/* count characters */
+
+			/* count characters */
+			++q->z;
 		}
-		if (argc > 1)					/* if not last argument... */
-		{
-			ptr.p->ch[ptr.c] = ' ';		/* space to separate arguments */
+
+		/* if not last argument... */
+		if (argc > 1) {
+
+			/* space to separate arguments */
+			ptr.p->ch[ptr.c] = ' ';
 			fwdcx(&ptr);
 			++q->z;
 		}
@@ -182,13 +226,21 @@ read_startup()
 
 	/* look for ".tecorc" in current directory first */
 
-	if (!(eisw = fopen(&startup_name[1], "r")))
-	{
-		if (hp = getenv("HOME"))	/* if not found, look in home directory */
-		{
-			for (i = 0; i < CELLSIZE; i++) if (!(fn[i] = *(hp++))) break;	/* copy until trailing null */
-			for (hp = &startup_name[0]; i < CELLSIZE; i++) if (!(fn[i] = *(hp++))) break;
-			eisw = fopen(fn, "r");		/* set eisw if file found, or not if not */
+	if (!(eisw = fopen(&startup_name[1], "r"))) {
+
+		/* if not found, look in home directory */
+		if (hp = getenv("HOME")) {
+
+			/* copy until trailing null */
+			for (i = 0; i < CELLSIZE; i++)
+				if (!(fn[i] = *(hp++)))
+					break;
+			for (hp = &startup_name[0]; i < CELLSIZE; i++)
+				if (!(fn[i] = *(hp++)))
+					break;
+
+			/* set eisw if file found, or not if not */
+			eisw = fopen(fn, "r");
 		}
 	}
 }
